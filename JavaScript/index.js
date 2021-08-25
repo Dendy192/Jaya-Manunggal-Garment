26822be24854909f64f854c35ce5cd4f62382507
Dendy192 <72143596+Dendy192@users.noreply.github.com>
2021-08-26 06:46:01 +0700

    Add files via upload

---
+++ JavaScript/index.js
@@ -0,0 +1,8 @@
+function myFunction(){
+
+    var button = document.getElementById('myButton')
+    var bsButton = new bootstrap.Button(button)
+    var bt1 = document.querySelector('#btn1')
+    var bt2 = document.querySelector('#btn2')
+    var bt3 = document.querySelector('#btn3')
+}
